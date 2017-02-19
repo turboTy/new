11d331e93aadf7464c389920594507a98286059f
turboTy <845383047@qq.com>
2017-02-19 19:46:02 +0800

    update-test.php
    二〇一七年二月十九日 19:45:37
    测试php页面美化

--- lib/page.func.php
+++ lib/page.func.php
@@ -35,14 +35,14 @@ function showPage($page,$totalPage,$where=null,$sep="&nbsp;"){
     $prevPage = ($page >= 1) ? $page - 1 : $page;
     $nextPage = ($page >= $totalPage) ? $totalPage : $page + 1;
     $totalText = "总共" . $totalPage . "页/当前第{$page}页";
-    $index = ($page == 1) ? "首页" : "<a href='{$url}?page=1{$where}'>首页</a>";
-    $prev = ($page == 1) ? "上一页" : "<a href='{$url}?page={$prevPage}{$where}'>上一页</a>";
-    $next = ($page == $totalPage) ? "下一页" : "<a href='{$url}?page={$nextPage}{$where}'>下一页</a>";
-    $last = ($page == $totalPage) ? "尾页" : "<a href='{$url}?page={$totalPage}{$where}'>尾页</a>";
+    $index = ($page == 1) ? "<span>首页</span>" : "<a href='{$url}?page=1{$where}'>首页</a>";
+    $prev = ($page == 1) ? "<span>上一页</span>" : "<a href='{$url}?page={$prevPage}{$where}'>上一页</a>";
+    $next = ($page == $totalPage) ? "<span>下一页</span>" : "<a href='{$url}?page={$nextPage}{$where}'>下一页</a>";
+    $last = ($page == $totalPage) ? "<span>尾页</span>" : "<a href='{$url}?page={$totalPage}{$where}'>尾页</a>";
     for ($i = 1; $i <= $totalPage; $i ++) {
         if ($i == $page) {
             // 当前页无链接
-            $p .= "[{$i}]";
+            $p .= "<b>[{$i}]</b>";
         } else {
             $p .= "<a href='{$url}?page={$i}{$where}'>[{$i}]</a>";
         }
