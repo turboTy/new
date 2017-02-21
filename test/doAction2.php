91c05097b9391765139aa2585102750cf64be7ab
turboTy <845383047@qq.com>
2017-02-21 14:22:45 +0800

    update-upload.func.php/uploadFiles()

--- test/doAction2.php
+++ test/doAction2.php
@@ -3,5 +3,5 @@ require_once '../include.php';
 require_once 'upload.func.php';
 header("content-type:text/html;charset=utf-8");
 // $info = $_FILES;
-$info = uploadInfo();
-var_dump($info);
+$fileinfo = uploadFiles();
+var_dump($fileinfo);
--- test/upload.func.php
+++ test/upload.func.php
@@ -72,6 +72,81 @@ function uploadFile($fileInfo,$maxSize = 1500000,$allowExt = array("gif","png","
 }
 
 /**上传多个文件
+ * @param string $path
+ * @param array $allowExt
+ * @param number $maxSize
+ * @param string $imgFlag
+ * @return string
+ */
+function uploadFiles($path='uploads',$allowExt=array("gif","jpeg","jpg","png","wbmp"),$maxSize=1500000,$imgFlag=true){
+    if (!file_exists($path)){
+        mkdir($path,0777,true);
+    }
+    $i = 0;
+    $files = uploadInfo();
+    foreach ($files as $fileinfo){
+        if ($fileinfo['error']===UPLOAD_ERR_OK){
+            //判断文件是否超出规定大小
+            if ($fileinfo['size'] > $maxSize){
+                exit("文件过大!");
+            }
+            $ext = getExt($fileinfo['name']);
+            $uniName = getUniName();
+            $filename = $uniName.".".$ext;
+            $destination = $path."/".$filename;
+            //判断后缀名
+            if (!in_array($ext, $allowExt)){
+                exit("非法文件名");
+            }
+            if ($imgFlag){
+                if (!getimagesize($fileinfo['tmp_name'])){
+                    exit("不是真正的图片文件");
+                }
+            }
+            if (is_uploaded_file($fileinfo['tmp_name'])){
+                if (move_uploaded_file($fileinfo['tmp_name'], $destination)){
+                    $fileinfo['name'] = $filename;
+                    unset($fileinfo['error'],$fileinfo['tmp_name'],$fileinfo['type']);
+                    $uploaded[$i] = $fileinfo;
+                    $i++;
+                }else{
+                    exit("文件移动失败");
+                }
+            }else{
+                exit("不是通过HTTP POST方式上传上来的");
+            }
+        }else{
+            switch ($fileinfo['error']){
+                case 1:
+                    $mes = "超过配置文件规定的上传文件的大小";//UPLOAD_ERR_INI_SIZE
+                    break;
+                case 2:
+                    $mes = "超过配置文件规定的表单大小"; //UPLOAD_ERR_FORM_SIZE
+                    break;
+                case 3:
+                    $mes = "文件部分被上传"; //UPLOAD_ERR_PARTIAL
+                    break;
+                case 4:
+                    $mes = "没有文件"; //UPLOAD_ERR_NO_FILE
+                    break;
+                case 6:
+                    $mes = "没有临时目录";//UPLOAD_ERR_NO_TMP_DIR;
+                    break;
+                case 7:
+                    $mes = "文件不可写"; //UPLOAD_ERR_CANT_WRITE
+                    break;
+                case 8:
+                    $mes = "由于PHP的扩展程序终止了上传"; //UPLOAD_ERR_EXTENSION
+                    break;
+            }
+            echo $mes;
+        }
+    }
+    return $uploaded;
+}
+
+
+/**构建上传文件信息
  * @return array
  */
 function uploadInfo(){
